de91b1b169eb0e3c21c3950ac3be8dd4b95ac5b9
Serge Borbit <sergeborbit@Serges-MBP.Dlink>
2015-09-13 14:04:21 +0300

    dependencies

--- gulpfile_compile.js
+++ gulpfile_compile.js
@@ -4,7 +4,7 @@ var gulp = require('gulp')
 var gulpLess = require('gulp-less')
 var gulpBase64 = require('gulp-base64')
 var gulpPostcss = require('gulp-postcss')
-var autoprefixer = require('autoprefixer-core')
+var autoprefixer = require('autoprefixer')
 var browserify = require('browserify')
 var babelify = require('babelify')
 
